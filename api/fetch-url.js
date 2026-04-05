c9a3b3f5523011b2e23c1bcd091e5d3d978f285f
emilielebret1981-maker <emilielebret1981@gmail.com>
2026-04-05 22:34:38 +0200

    Create fetch-url.js

---
+++ api/fetch-url.js
@@ -0,0 +1,6 @@
+const https = require('https');
+module.exports = async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') return res.status(200).end();
+  res.status(200).json({ content: 'test OK' });
+};
